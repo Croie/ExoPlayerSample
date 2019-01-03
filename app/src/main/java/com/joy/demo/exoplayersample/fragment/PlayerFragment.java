ab13a5cd422ba63afca687d66671d4a1e063ad12
lixin <729067698@qq.com>
2019-01-03 10:49:16 +0800

    增加 PlayerFragment PlayManager Playback res/layout/fragment_player.xml

---
+++ app/src/main/java/com/joy/demo/exoplayersample/fragment/PlayerFragment.java
@@ -0,0 +1,10 @@
+package com.joy.demo.exoplayersample.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by lhf on 2018/12/14.
+ */
+
+public class PlayerFragment extends Fragment {
+}
---
+++ app/src/main/java/com/joy/demo/exoplayersample/playback/Playback.java
@@ -0,0 +1,8 @@
+package com.joy.demo.exoplayersample.playback;
+
+/**
+ * Created by lhf on 2018/12/28.
+ */
+
+public interface Playback {
+}
